236598b40b63985e2744befded48dc78d4593a5a
hrparkdev <hrparkdev@gmail.com>
2022-03-04 23:01:37 +0900

    Update index.js

--- username/index.js
+++ username/index.js
@@ -1,12 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector(".login-input");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "nsername";
+
 function appearUserName() {
-  const getUserName = localStorage.getItem("username");
+  const getUserName = localStorage.getItem(USERNAME_KEY);
   if (getUserName === null) {
-    loginForm.classList.remove("hidden");
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
   } else if (getUserName !== null) {
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const newDiv = document.createElement("div");
     const newContent = document.createTextNode(`Hello, ${getUserName}`);
     newDiv.appendChild(newContent);
@@ -18,7 +21,7 @@ appearUserName();
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const userName = loginInput.value;
-  localStorage.setItem("username", userName);
+  localStorage.setItem(USERNAME_KEY, userName);
   appearUserName();
 });
 
