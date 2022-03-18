464f6b4952a36576616c0cad86fc8bdf2e3060ab
hrparkdev <hrparkdev@gmail.com>
2022-03-18 22:10:30 +0900

    Event keycodes

---
+++ event-keycodes/index.js
@@ -0,0 +1,8 @@
+const key = document.querySelector("#insert");
+
+window.addEventListener("keydown", (event) => {
+  const alphabet = event.key;
+  const upperCase = alphabet.toUpperCase();
+  const ascii = upperCase.charCodeAt(0);
+
+});
