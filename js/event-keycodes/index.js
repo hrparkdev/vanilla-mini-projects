62ea8be8ddaa71db42f401cce6191958dbe202ad
hrparkdev <hrparkdev@gmail.com>
2022-04-30 23:57:44 +0900

    Update index.js

--- js/event-keycodes/index.js
+++ js/event-keycodes/index.js
@@ -5,5 +5,11 @@ window.addEventListener("keydown", (event) => {
     <div class="key">
       ${event.key === " " ? "Space" : event.key}
       <small>event.key</small>
+    </div>
+
+    <div class="key">
+      ${event.keyCode}
+      <small>event.keyCode</small>
+    </div>
   `
 });
